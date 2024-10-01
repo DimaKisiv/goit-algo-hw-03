405b4b611047c7bcbcaf2e1abf98afb2fdad346e
dimakisiv <dimakisiv@gmail.com>
2024-10-01 18:53:57 +0300

    added exceptions

--- task1.py
+++ task1.py
@@ -15,34 +15,43 @@ def parse_arguments():
 def collect_files_recursively(source_dir, files = None):
     if files is None:
         files = []
-    source_path = Path(source_dir)
-    files_and_folders = list(source_path.iterdir())
-    for item in files_and_folders:
-        if (item.is_file):
-            files.append(item)
-        if (item.is_dir()):
-            collect_files_recursively(item, files)
+    try:
+        source_path = Path(source_dir)
+        files_and_folders = list(source_path.iterdir())
+        for item in files_and_folders:
+            if (item.is_file):
+                files.append(item)
+            if (item.is_dir()):
+                collect_files_recursively(item, files)
+    except Exception as e:
+        print(e)
     return files
 
 def copy_files(files, dest_dir):
     extensions = set([f.suffix[1:] for f in files])
     extension_folders = create_extensions_folders(extensions, dest_dir)
     for f in files:
-        file_ext = f.suffix[1:]
-        if (file_ext == ''):
-            continue
-        file_path_dest = extension_folders[file_ext] / f.name
-        if (not file_path_dest.exists()):
-            shutil.copy2(f, file_path_dest)
+        try:
+            file_ext = f.suffix[1:]
+            if (file_ext == ''):
+                continue
+            file_path_dest = extension_folders[file_ext] / f.name
+            if (not file_path_dest.exists()):
+                shutil.copy2(f, file_path_dest)
+        except Exception as e:
+            print(e)
 
 
 def create_extensions_folders(extensions, dest_dir):
     extension_folders = {}
-    dest_path = Path(dest_dir)
-    for ext in [e for e in extensions if e != '']:
-        folder_path = dest_path / ext 
-        folder_path.mkdir(parents=True, exist_ok=True)
-        extension_folders[ext] = folder_path
+    try:
+        dest_path = Path(dest_dir)
+        for ext in [e for e in extensions if e != '']:
+            folder_path = dest_path / ext 
+            folder_path.mkdir(parents=True, exist_ok=True)
+            extension_folders[ext] = folder_path
+    except Exception as e:
+        print(e)
     return extension_folders
 
 def main():
